6002f3817f5b1909b9d429190c728a29bd9c2bc6
Maroch57 <jahathblessed@gmail.com>
2024-01-05 16:02:59 +0300

    functions loops and if-else stats in python

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -13,4 +13,4 @@ elif lasdig == 0:
     print(f"Last digit of {number:d} is {lasdig:d} and is 0")
 else:
     print(f"Last digit of {number:d} is {lasdig:d} and is \
-\   \   \   less than 6 and not 0")
+less than 6 and not 0")
